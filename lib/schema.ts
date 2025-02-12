a663172f56edace9a4fe6d1c11d425c2ea5d83ee
ROSA WU <rosawu@college.harvard.edu>
2025-02-11 20:55:38 -0500

    updated schema.ts after adding a comments table in supabase

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,19 +1,40 @@
-export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
 
 export interface Database {
   public: {
     Tables: {
       profiles: {
-        Row: { biography: string | null; display_name: string; email: string; id: string };
-        Insert: { biography?: string | null; display_name: string; email: string; id: string };
-        Update: { biography?: string | null; display_name?: string; email?: string; id?: string };
+        Row: {
+          biography: string | null;
+          display_name: string;
+          email: string;
+          id: string;
+        };
+        Insert: {
+          biography?: string | null;
+          display_name: string;
+          email: string;
+          id: string;
+        };
+        Update: {
+          biography?: string | null;
+          display_name?: string;
+          email?: string;
+          id?: string;
+        };
         Relationships: [
           {
             foreignKeyName: "profiles_id_fkey";
             columns: ["id"];
             referencedRelation: "users";
             referencedColumns: ["id"];
-          },
+          }
         ];
       };
       species: {
@@ -56,13 +77,55 @@ export interface Database {
             columns: ["author"];
             referencedRelation: "profiles";
             referencedColumns: ["id"];
+          }
+        ];
+      };
+      comments: {
+        Row: {
+          id: number;
+          species_id: number;
+          user_id: string;
+          comment_text: string;
+          created_at: string;
+          updated_at: string;
+        };
+        Insert: {
+          id?: number;
+          species_id: number;
+          user_id: string;
+          comment_text: string;
+          created_at?: string;
+          updated_at?: string;
+        };
+        Update: {
+          id?: number;
+          species_id?: number;
+          user_id?: string;
+          comment_text?: string;
+          created_at?: string;
+          updated_at?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "comments_species_id_fkey";
+            columns: ["species_id"];
+            referencedRelation: "species";
+            referencedColumns: ["id"];
           },
+          {
+            foreignKeyName: "comments_user_id_fkey";
+            columns: ["user_id"];
+            referencedRelation: "profiles";
+            referencedColumns: ["id"];
+          }
         ];
       };
     };
     Views: { [_ in never]: never };
     Functions: { [_ in never]: never };
-    Enums: { kingdom: "Animalia" | "Plantae" | "Fungi" | "Protista" | "Archaea" | "Bacteria" };
+    Enums: {
+      kingdom: "Animalia" | "Plantae" | "Fungi" | "Protista" | "Archaea" | "Bacteria";
+    };
     CompositeTypes: { [_ in never]: never };
   };
 }
